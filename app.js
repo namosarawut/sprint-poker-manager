cd859c9d97cea59c5a79782051940f13daeee14b
namosarawut <namosarawut02@gmail.com>
2025-02-08 20:33:12 +0700

    change port to 10000

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => console.log('Server is running on port 3000'));
+server.listen(10000, () => console.log('Server is running on port 10000'));
  
